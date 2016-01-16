cf77d4eea15e279c7b1b3fddc6592fdc531fe184
buddhikaw <bnwimalasena@live.com>
2016-01-16 14:36:39 +0800

    Initial Commit

---
+++ Buddhika.Google.Blogger.Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Buddhika.Google.Blogger.util;
+
+namespace Buddhika.Google.Blogger.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            util.Blogger access = new util.Blogger();
+            Task task = new Task(access.Authentication);
+            task.Start();
+            task.Wait();
+            access.ServiceOpen();
+            long totalPageViews = access.GetPageViews();
+        }
+    }
+}
---
+++ Buddhika.Google.Blogger.util/Blogger.cs
@@ -0,0 +1,62 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Auth.OAuth2.Responses;
+using Google.Apis.Blogger.v3;
+using Google.Apis.Blogger.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Buddhika.Google.Blogger.util
+{
+    public class Blogger
+    {
+        public UserCredential credential { get; set; }
+        public BloggerService service { get; set; }
+
+        public async void Authentication()
+        {
+            try
+            {
+                using (var stream = new FileStream("client_secrets.json", FileMode.Open, FileAccess.Read))
+                {
+                    credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                        GoogleClientSecrets.Load(stream).Secrets,
+                        new[] { BloggerService.Scope.Blogger },
+                        "user", CancellationToken.None, new FileDataStore("Blogger.util"));
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+
+        }
+
+        public void ServiceOpen()
+        {
+            service = new BloggerService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "Buddhika Blogger",
+            });
+        }
+
+        public long GetPageViews()
+        {
+            long? count;
+            var p = service.PageViews.Get("2590723630599333470");
+            p.Range = PageViewsResource.GetRequest.RangeEnum.All;
+            Pageviews response = p.Execute();
+            count = response.Counts[0].Count;
+            return count ?? 0;
+            //StreamReader reader = new StreamReader(str);
+            //string text = reader.ReadToEnd();
+        }
+    }
+}
